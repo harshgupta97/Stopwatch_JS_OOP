ba787d996f955b3521548ffa8dfb3941cf388843
Harsh Gupta <warmachine476@gmail.com>
2020-08-29 20:53:12 +0530

    first commit with implementation

---
+++ index.js
@@ -0,0 +1,39 @@
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function () {
+        if (running)
+            throw new Error('Stopwatch is already running');
+
+        running = true;
+
+        startTime = new Date();
+    }
+
+    this.stop = function () {
+        if (!running)
+            throw new Error("Stopwatch is not running.");
+
+        running = false;
+
+        endTime = new Date();
+
+        const second = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += second;
+    }
+
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    }
+
+    Object.defineProperty(this, 'duration', {
+        get: function () {
+            return duration;
+        }
+    })
+}
+
+const sw = new Stopwatch();
